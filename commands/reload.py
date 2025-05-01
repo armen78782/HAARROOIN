77f2e7f1bc84b41219b7a7ec4969489590dd2845
armen78782 <112015796+armen78782@users.noreply.github.com>
2025-05-01 23:07:59 +0400

    Delete commands/reload.py

--- commands/reload.py
+++
@@ -1,9 +0,0 @@
-async def reload(ctx):
-    """Перезагружает все команды из папки 'commands'"""
-    for filename in os.listdir('./commands'):
-        if filename.endswith('.py') and filename != 'reload.py':  # исключаем лоадер
-            try:
-                bot.reload_extension(f'commands.{filename[:-3]}')  # Перезагружаем команду
-                await ctx.send(f'Команда {filename} перезагружена!')
-            except Exception as e:
-                await ctx.send(f'Ошибка при перезагрузке {filename}: {e}')
