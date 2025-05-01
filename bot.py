7d6142818805dc1961239e74f97e79d93b66d902
armen78782 <112015796+armen78782@users.noreply.github.com>
2025-05-01 23:58:49 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def load_extensions():
     for filename in os.listdir('./commands'):
         if filename.endswith('.py') and filename != 'loader.py':  # исключаем лоадер
             try:
-                bot.load_extension(f'commands.{filename[:-3]}')  # Загружаем команду (без await)
+                await bot.load_extension(f'commands.{filename[:-3]}')  # Загружаем команду (без await)
                 print(f"Команда {filename} загружена.")
             except Exception as e:
                 print(f"Ошибка при загрузке команды {filename}: {e}")
@@ -24,7 +24,7 @@ async def reload(ctx):
     for filename in os.listdir('./commands'):
         if filename.endswith('.py') and filename != 'reload.py':  # исключаем лоадер
             try:
-                bot.reload_extension(f'commands.{filename[:-3]}')  # Перезагружаем команду (без await)
+                await bot.reload_extension(f'commands.{filename[:-3]}')  # Перезагружаем команду (без await)
                 await ctx.send(f'Команда {filename} перезагружена!')
             except Exception as e:
                 await ctx.send(f'Ошибка при перезагрузке {filename}: {e}')
